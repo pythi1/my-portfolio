8129c4ed97b182dfec1462f0830fb6cb8f3ecfd9
Pramod <pramodjangra144@gmail.com>
2025-07-13 23:47:42 +0530

    adding icons

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -47,32 +47,31 @@ const TechStack = () => {
               The key is set to the name of the tech stack icon, and the classnames are set to 
               card-border, tech-card, overflow-hidden, and group. The xl:rounded-full and rounded-lg 
               classes are only applied on larger screens. */}
-          {techStackIcons.map((techStackIcon) => (
+          {/* {techStackIcons.map((techStackIcon) => (
             <div
               key={techStackIcon.name}
               className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
             >
               {/* The tech-card-animated-bg div is used to create a background animation when the 
                   component is hovered. */}
-              <div className="tech-card-animated-bg" />
+              {/* <div className="tech-card-animated-bg" />
               <div className="tech-card-content">
                 {/* The tech-icon-wrapper div contains the TechIconCardExperience component, 
                     which renders the 3D model of the tech stack icon. */}
-                <div className="tech-icon-wrapper">
+                {/* <div className="tech-icon-wrapper">
                   <TechIconCardExperience model={techStackIcon} />
                 </div>
                 {/* The padding-x and w-full classes are used to add horizontal padding to the 
                     text and make it take up the full width of the component. */}
-                <div className="padding-x w-full">
+                {/* <div className="padding-x w-full">
                   {/* The p tag contains the name of the tech stack icon. */}
-                  <p>{techStackIcon.name}</p>
+                  {/* <p>{techStackIcon.name}</p>
                 </div>
               </div>
-            </div>
-          ))}
+            </div> */} 
 
           {/* This is for the img part */}
-          {/* {techStackImgs.map((techStackIcon, index) => (
+          {techStackImgs.map((techStackIcon, index) => (
             <div
               key={index}
               className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
@@ -87,7 +86,7 @@ const TechStack = () => {
                 </div>
               </div>
             </div>
-          ))} */}
+          ))}
         </div>
       </div>
     </div>
